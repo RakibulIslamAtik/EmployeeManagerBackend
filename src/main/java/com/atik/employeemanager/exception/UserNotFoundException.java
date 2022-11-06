f0d4496f6462734284f7553f9ed0e4bd3bef44de
RakibulIslamAtik <rakibul.islam.atik1@gmail.com>
2022-11-06 19:12:58 +0600

    test

--- src/main/java/com/atik/employeemanager/exception/UserNotFoundException.java
+++ src/main/java/com/atik/employeemanager/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.atik.employeemanager.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String massage) {
-        super(massage);
+    public UserNotFoundException(String message) {
+        super(message);
     }
 }
--- src/main/java/com/atik/employeemanager/service/EmployeeService.java
+++ src/main/java/com/atik/employeemanager/service/EmployeeService.java
@@ -31,7 +31,7 @@ public class EmployeeService {
     }
     public Employee findEmployeeById(Long id){
         return employeeRepo.findEmployeeById(id)
-                .orElseThrow(() -> new UserNotFoundException("User by id " +id+ " was not found" ));
+                .orElseThrow(() -> new UserNotFoundException("User by id " +id+ " was  not found" ));
     }
     public void deleteEmployee(Long id){
        employeeRepo.deleteEmployeeById(id);
